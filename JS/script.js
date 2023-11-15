180f3ee9334b831048540541482e407cb565f0a0
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2023-11-15 20:48:54 +0100

    feat generate password

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+// Chiediamo il suo nome
+
+const your_name = prompt("what's your name?");
+console.log(your_name);
+
+// Chiedere il suo cognome
+
+const your_last_name = prompt("what's your last name?");
+console.log(your_last_name);
+
+// Chiedere il colore preferito
+
+const your_favourite_colour = prompt("What's your favourite colour?");
+console.log(your_favourite_colour);
+
+// Creazione password
+
+const user_password = your_name + your_last_name + your_favourite_colour + 23;
+
+console.log(user_password);
+
+// Far apparire in pagina la password dell'utente
+
+const messageElement = document.getElementById("password");
+
+messageElement.innerHTML = `la tua password: ${user_password}`;
